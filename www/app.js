6656ec97d1ba13c703aeeed953cc083700395877
Werdon4 <filipgajewski4@gmail.com>
2020-03-17 16:07:51 +0100

    Filip poprawki
    Nowy plik app.js i struktura katalogów.
    Na linuxie działa serwer, html i css strony
    wraz ze zdjeciami wczytuje się pooprawnie.

---
+++ www/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname+'/public'));
+// app.use(express.static(__dirname+'/public'));
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(3000);
